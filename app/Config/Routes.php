2ea4cd69728a13b02473c6546dfed2c317e648a7
claraztn <claraztn@example.com>
2024-12-26 16:54:44 +0700

    ini salah

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,11 +8,11 @@ use CodeIgniter\Router\RouteCollection;
 
 $routes->get('/', 'Home::index');
 
-$routes->group('auth', ['namespace' => 'Modules\Auth\Controllers'], function ($routes) {
+$routes->group('', ['namespace' => 'Modules\Auth\Controllers'], function ($routes) {
   $routes->get('/', 'Auth::index');
   $routes->post('login', 'AuthController::login');
 });
 
-$routes->group('user', ['namespace' => 'Modules\User\Controllers'], function ($routes) {
+$routes->group('', ['namespace' => 'Modules\User\Controllers'], function ($routes) {
   $routes->get('dashboard', 'User::index');
 });
